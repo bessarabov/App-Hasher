baa01bd11e035081f2545bddd1ad408537cf6191
Ivan Bessarabov <ivan@bessarabov.ru>
2016-05-23 23:03:38 +0300

    Initial commit

---
+++ lib/App/Hasher.pm
@@ -0,0 +1,22 @@
+package App::Hasher;
+
+use strict;
+use warnings FATAL => 'all';
+use feature 'say';
+use utf8;
+use open qw(:std :utf8);
+
+# ABSTRACT: script to change file name to be hash of its content
+
+=head1 SYNOPSIS
+
+Please read L<hasher> documentation.
+
+=head1 DESCRIPTION
+
+App::IsGitSynced version numbers uses Semantic Versioning standart.
+Please visit L<http://semver.org/> to find out all about this great thing.
+
+=cut
+
+1;
